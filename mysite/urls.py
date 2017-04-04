9d4b1b1690f9f9a58b4300dea45e4279ae51efb4
rune <rune.hartvig@outlook.com>
2017-04-04 21:46:47 +0200

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from mysite.views import hello
 from mysite.views import current_datetime
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})$', hours_ahead),
+    url(r'', include('blog.urls')),
 ]
